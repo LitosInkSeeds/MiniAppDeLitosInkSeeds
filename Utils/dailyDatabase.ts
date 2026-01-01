fa8d5915da94e4be8cc3faca0b222df884778bcd
LitosInkSeeds <carlosmendietamartinez@outlook.es>
2026-01-01 22:58:38 +0100

    Create dailyDatabase.ts

---
+++ Utils/dailyDatabase.ts
@@ -0,0 +1,5 @@
+
+export const BREATH_DB = Array(28).fill({name: "Respiración Consciente", desc: "Enfócate en tu flujo vital."}).map((v, i) => ({name: `Técnica #${i+1}`, desc: v.desc}));
+export const STRETCH_DB = Array(28).fill({name: "Estiramiento Solar", desc: "Abre tus canales energéticos."});
+export const GYM_DB = Array(28).fill({name: "Entreno Natural", desc: "Mueve tu cuerpo como fue diseñado."});
+export const QUOTES_SPIRIT_DB = Array(28).fill(["Busca el silencio y encontrarás la luz.", "El presente es el único regalo.", "La paz nace desde adentro."]);
